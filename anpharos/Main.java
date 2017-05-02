2ef6afd082dfac632b2a20ccf36eff91fa8098e5
José Alberto Jurado <juradohja@gmail.com>
2017-05-01 19:44:03 -0500

    Returned to 8e251cf keeping pacakge structure

--- anpharos/Main.java
+++ anpharos/Main.java
@@ -8,61 +8,7 @@ import javax.swing.*;
 import java.util.Hashtable;
 public class Main {
 
-    public static JFrame ventana;
-
-    private static Hashtable<Integer, SingleLinkedList<String>> hashTable;
-    private static SingleLinkedList<String> password;
-
     public static void main(String[] strings)
     {
-        password = new SingleLinkedList<>();
-        hashTable= new Hashtable<>();
-        password.addFirst("usuario");
-        hashTable.put("usuario".hashCode(),password);
-        ventana = new JFrame();
-        ventana.setTitle("Proyecto Sphero");
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setSize(1000,1000);
-        ventana.setContentPane(new Registro().getLogIn());
-        ventana.setVisible(true);
-
-    }
-
-    public static void entrar()
-    {
-       Anpharos a = new Anpharos();   //Aquí se tiene que abrir la ventana de Anpharos
-        ventana.setVisible(true);
-
-    }
-
-    public static void agregarTabla(String texto, char[] contra)
-    {
-        String password = new String(contra);
-
-        if(hashTable.get(texto.hashCode())!=null) {
-            hashTable.get(texto.hashCode()).addLast(password);
-        }
-        else
-        {
-           SingleLinkedList<String> lista = new SingleLinkedList<>();
-            lista.addLast(password);
-            hashTable.put(texto.hashCode(), lista);
-        }
-    }
-
-    public static SingleLinkedList<String> getUsuario(int usuario)
-    {
-        if(hashTable.get(usuario) == null) {
-            SingleLinkedList<String> vacio = new SingleLinkedList<>();
-            vacio.addLast("");
-            return vacio;
-        }
-        return hashTable.get(usuario);
-    }
-
-    public static Hashtable<Integer,SingleLinkedList<String>> gethashTable()
-    {
-        return hashTable;
-    }
-
+       Anpharos a = new Anpharos(); 
 }
